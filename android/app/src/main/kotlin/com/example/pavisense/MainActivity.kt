1f7c92403184004eb08bbbff095c7d80a525c90e
gabrielmoliva <moliva.gabriel@gmail.com>
2025-06-09 09:06:42 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/com/example/pavisense/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pavisense
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: const Text('Texto radicarlos'),
+        ),
+        body: Stack(
+          
+        ),
+        
+      )
+    );
+  }
+}
